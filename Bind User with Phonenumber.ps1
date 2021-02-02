7792ab500ec6cd86e2cf3f45db792be54097f567
Johnny Winkler <johnnyf.winkler@hotmail.com>
2021-02-02 16:58:48 +0100

    Create Bind User with Phonenumber.ps1

---
+++ Bind User with Phonenumber.ps1
@@ -0,0 +1,10 @@
+﻿# Verbindung mit Teams Connector herstellen
+Connect-MicrosoftTeams -Credential $O365cred
+$session = New-CsOnlineSession -Credential $O365cred
+Import-PsSession $session
+
+# Telefonnummer mit Ressource verknüpfen
+Set-CsOnlineApplicationInstance -Identity [ressource]@[initialdomain].onmicrosoft.com -OnpremPhoneNumber +41xxxxxxxxx
+
+# Telefonnummer mit User verknüpfen
+Set-CsUser -identity “[UPN]” -EnterpriseVoiceEnabled $true -HostedVoicemail $true -OnPremlineURI tel:+41xxxxxxxxx
