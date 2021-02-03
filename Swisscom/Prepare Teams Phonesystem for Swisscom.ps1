e1f4403c756a1c5e398c4097afb7b5b6480e6de1
Johnny Winkler <johnnyf.winkler@hotmail.com>
2021-02-03 08:35:33 +0100

    Update Prepare Teams Phonesystem for Swisscom.ps1

--- Swisscom/Prepare Teams Phonesystem for Swisscom.ps1
+++ Swisscom/Prepare Teams Phonesystem for Swisscom.ps1
@@ -7,6 +7,6 @@ Set-CsOnlinePstnUsage -Identity Global -Usage @{Add="SwisscomET4T"}
 New-CsOnlineVoiceRoute -Identity "SwisscomET4T_Unrestricted" -NumberPattern ".*" -OnlinePstnGatewayList "[Domainname]" -OnlinePstnUsages "SwisscomET4T"
 New-CsOnlineVoiceRoutingPolicy "SwisscomET4T" -OnlinePstnUsages "SwisscomET4T"
 
-# Gemäss Swisscom rund 15 Minuten warten. Anschliessend kann gemäss dem PowerShell Script "Bind User with Phonenumer" weitergefahren werden
+# Gemäss Swisscom rund 15 Minuten warten. Anschliessend kann gemäss dem PowerShell Script "Bind User with Phonenumber" weitergefahren werden
 
 Grant-CsOnlineVoiceRoutingPolicy -Identity [E-Mail User] -PolicyName "SwisscomET4T"
